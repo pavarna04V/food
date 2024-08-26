339823fdf982acf9240d3b8477dbe00a17f30e9d
pavarna04V <pavarna04_bai26@mepcoeng.ac.in>
2024-08-26 10:20:17 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://food-frontend-5h8w.onrender.com"
 
   return (
     <div>
